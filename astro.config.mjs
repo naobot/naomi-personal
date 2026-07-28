aef221bd8d4092e1191408e75c465f32f286664f
naobot <naomi@nowme.ca>
2026-07-28 15:04:48 +0900

    wire up vercel/microfrontends framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { microfrontends } from '@vercel/microfrontends/experimental/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [microfrontends()],
+  },
+});
